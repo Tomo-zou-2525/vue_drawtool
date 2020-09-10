aeba9b8c711d568db5baf9203525e4555a4faab7
友造-2525 <bathbook1990tomozou@gmail.com>
2020-09-10 17:01:48 +0900

    feat:描画機能の追加・canvas要素と各種イベントの追加

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <DrawTool />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import DrawTool from "./components/DrawTool.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // HelloWorld,
+    DrawTool
   }
-}
+};
 </script>
 
 <style>
--- src/components/DrawTool.vue
+++ src/components/DrawTool.vue
@@ -1,5 +1,50 @@
 <template>
   <div>
     <h1>DrawTool</h1>
+    <div id="canvas-area">
+      <canvas
+        id="draw-area"
+        width="500px"
+        height="500px"
+        @mousedown="paintS"
+        @mouseup="paintE"
+        @mouseout="paintE"
+        @mousemove="draw"
+      ></canvas>
+    </div>
   </div>
 </template>
+
+<script>
+// scriptの構文
+export default {
+  // コンポーネントの指定
+  name: "DrawTool",
+  // 使用するデータの定義
+  data() {
+    // なぜリターン？→data()が関数となっているので、戻り値を返す必要があるため
+    return {
+      canvas: null,
+      context: null,
+      isDrag: false
+    };
+  },
+  mounted() {
+    // canvas要素の取得（マウント）
+    this.canvas = document.querySelector("#draw-area");
+    // canvasの画像要素指定
+    this.context = this.canvas.getContext("2d");
+    // 線の端に丸みを帯びさせる・canvasの要素
+    this.context.lineCap = "round";
+    this.context.lineCap = "round";
+    this.context.lineCap = "round";
+    this.context.lineCap = "round";
+  }
+};
+</script>
+
+<style scoped>
+#drew-area {
+  border: 1px, solid, #000;
+}
+</style>>
