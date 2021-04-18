c103207feb1483f6dce795bbfdadaefbf24a22d2
友造-2525 <bathbook1990tomozou@gmail.com>
2021-04-19 04:35:34 +0900

    vue router追加

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <p><VueSwatche /></p>
-    <p><DrawTool /></p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import DrawTool from "./components/DrawTool.vue";
-import VueSwatche from "./components/VueSwatche";
-
-export default {
-  name: "App",
-  components: {
-    // HelloWorld,
-    DrawTool,
-    VueSwatche,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -26,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
